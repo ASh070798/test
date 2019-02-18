8bd8f413c60ec0264ceb715250709bc5d73ffe01
ASh070798 <47598315+ASh070798@users.noreply.github.com>
2019-02-18 11:37:46 +0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Bags = new int[] { 4, 9, 3, 1, 6, 2, 5, 7, 8 }; // номера пакетов
+            int max = Bags[0];
+            int min = Bags[0];
+            bool exist = false;
+            max = Bags.Max();
+            min = Bags.Min();
+            for (int i = min+1; i < max; i++)
+            {
+                for (int j = 0; j < Bags.Length; j++)
+                {
+                    if (i == Bags[j])
+                    {
+                        exist = true;
+                        break;
+                    }
+                    else
+                        exist = false;
+                }
+                if (exist == false)
+                {
+                    Console.WriteLine("False!"); // не все пакеты дошли
+                    return;
+                }     
+            }
+            Console.WriteLine("Successfuly!"); // дошли все пакеты
+        }
+    }
+}
